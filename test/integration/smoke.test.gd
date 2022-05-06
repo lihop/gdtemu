8d330217c7008d8abeb77df0342ecf931a6eab3c
Leroy Hopson <git@leroy.geek.nz>
2022-05-06 14:26:46 +0700

    Update GitHub actions workflow
    - Build debug and release binaries.
    - Cache godot-cpp builds.
    - Run unit and integration tests.
    - Run linux build inside a Docker container with an older version of
      GLIBC. This results in the newest GLIBC symbol being version 2.17
      which was released in 2012 and allows users with older versions of
      GLIBC installed on their system to run the library.

---
+++ test/integration/smoke.test.gd
@@ -0,0 +1,16 @@
+extends "res://addons/gut/test.gd"
+
+
+func test_run_pc():
+	# TODO: Implement me.
+	pass
+
+
+func test_run_riscv32():
+	# TODO: Implement me.
+	pass
+
+
+func test_run_riscv64():
+	# TODO: Implement me.
+	pass
