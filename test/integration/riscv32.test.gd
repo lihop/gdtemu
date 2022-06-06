931d0ad87b1c56788499d5ebfbe06ecb42e8ffb2
Leroy Hopson <git@leroy.geek.nz>
2022-06-06 22:56:45 +0700

    Move Berkeley boot loader files to addons directory
    Moves riscv32/riscv64 Berkeley boot loader binaries (`bbl32.bin`/`bbl64.bin`)
    to the `addons/gdtemu/native/bin` directory so they can easily be used
    by other projects.

--- test/integration/riscv32.test.gd
+++ test/integration/riscv32.test.gd
@@ -4,7 +4,7 @@ extends "./test_base.gd"
 static func setup_vm(vm, use_threads := false) -> void:
 	.setup_vm(vm, use_threads)
 	vm.config.machine_class = vm.config.MACHINE_CLASS_RISCV32
-	vm.config.bios = "res://examples/riscv32/bbl32.bin"
+	vm.config.bios = "res://addons/gdtemu/native/bin/bbl32.bin"
 	vm.config.kernel = "res://examples/riscv32/images/Image"
 	vm.config.cmdline = "loglevel=1 printk.time=0 console=hvc0"
 
--- test/integration/riscv64.test.gd
+++ test/integration/riscv64.test.gd
@@ -4,7 +4,7 @@ extends "./test_base.gd"
 static func setup_vm(vm, use_threads := false) -> void:
 	.setup_vm(vm, use_threads)
 	vm.config.machine_class = vm.config.MACHINE_CLASS_RISCV64
-	vm.config.bios = "res://examples/riscv64/bbl64.bin"
+	vm.config.bios = "res://addons/gdtemu/native/bin/bbl64.bin"
 	vm.config.kernel = "res://examples/riscv64/images/Image"
 	vm.config.cmdline = "loglevel=1 printk.time=0 console=hvc0"
 
